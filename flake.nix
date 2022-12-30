0ff347e3554542261ed3d7cff0e5ae28b56732e3
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-12-30 15:41:16 +0300

    remove shit

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zig.url = "github:mitchellh/zig-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    zigmod-binary.url = "https://github.com/nektro/zigmod/releases/download/r84/zigmod-x86_64-linux";
-    zigmod-binary.flake = false;
   };
 
   outputs = {
@@ -17,16 +15,7 @@
       perSystem = {pkgs, system, ...}: {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.pkg-config
-            pkgs.gtk3
             zig.packages.${system}.master
-            (pkgs.runCommand "zigmod-r84" {
-              nativeBuildInputs = [pkgs.autoPatchelfHook];
-            } ''
-              mkdir -p $out/bin
-              cp ${inputs.zigmod-binary} $out/bin/zigmod
-              chmod +x $out/bin/zigmod
-            '')
           ];
         };
       };
--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,5 @@
 const std = @import("std");
-const capy = @import("capy");
-
-// This is required for your app to build to WebAssembly and other particular architectures
-pub usingnamespace capy.cross_platform;
 
 pub fn main() !void {
-    try capy.backend.init();
-    
-    var window = try capy.Window.init();
-    try window.set(
-        capy.Label(.{ .text = "Hello, World" })
-    );
-    
-    window.setTitle("Hello");
-    window.resize(250, 100);
-    window.show();
-    
-    capy.runEventLoop();
+    std.debug.print("aaaaaaaaaaaaaaaaaaaaaa", .{});
 }
