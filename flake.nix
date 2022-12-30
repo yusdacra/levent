2b262f684022ee88102857b904bc71a922a2494b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-12-30 08:49:43 +0300

    setup nix dev env

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    zigmod-binary.url = "https://github.com/nektro/zigmod/releases/download/r84/zigmod-x86_64-linux";
+    zigmod-binary.flake = false;
+  };
+
+  outputs = {
+    zig,
+    flake-parts,
+    ...
+  }@inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {pkgs, system, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            zig.packages.${system}.master
+            (pkgs.runCommand "zigmod-r84" {
+              nativeBuildInputs = [pkgs.autoPatchelfHook];
+            } ''
+              mkdir -p $out/bin
+              cp ${inputs.zigmod-binary} $out/bin/zigmod
+              chmod +x $out/bin/zigmod
+            '')
+          ];
+        };
+      };
+    };
+}
